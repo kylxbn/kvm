6aaa813f64e6a9e59945081f5c08fcfc9bf46859
Kyle Alexander Buan <me@kylxbn.com>
2021-09-17 11:42:16 +0800

    New bootloader and sample program

--- asm/bootloader.asm
+++ asm/bootloader.asm
@@ -1,50 +1,54 @@
-#offset $0000
+#offset $0000 ; this is the bootloader so we start at $0000
 
-START:
-    LD A $F0E9
-    ST A $00D0
+    LD A #$00     ; we read sector 0
+    ST A $F300    ; store high pointer
+    ST A $F301    ; store low pointer
+    ST A $F302    ; store 0 to read
 
-LOOP1:
-    LD A $00D0
-    LD X $F0E9
-    SB A X
-    JZ LOOP1
-
-    ; it's not equal
-    ST X $00D2
-
-    LD A $F0E9
-    ST A $00D1
-
-LOOP2:
-    LD A $00D1
-    LD X $F0E9
-    SB A X
-    JZ LOOP2
-
-    ; it's not equal
-    ST X $00D3
-
-    ; send stuff to GPU
-    LD A $00D0
+    LD A #$FF     ; set line color to white etc
+    ST A $F0F4
+    ST A $F0F5
+    ST A $F0F6
+    LD A #$00
     ST A $F0F0
-    LD A $00D1
-    ST A $F0F1
-    LD A $00D2
     ST A $F0F2
-    LD A $00D3
+    LD A #$80
+    ST A $F0F1
     ST A $F0F3
 
-    ; set color
-    LD A $F0E9
-    ST A $F0F4
-    LD A $F0E9
-    ST A $F0F5
-    LD A $F0E9
-    ST A $F0F6
+    LD X #00      ; so we start at offset zero
+LOOP:
+    ST X $F0F2
+    ST A $F0FF    ; draw a white line
 
-    ; draw
+    LD A $F100+X  ; and we read HDD RAM with offset X
+    ST A $0300+X  ; then write that to $0300 with offset X
+
+    ; we increment X
+    ST X $0100    ; save X to $0100
+    LD A $0100    ; put that to A
+    AD A #01      ; add 1 to A
+    
+    JZ END        ; if that is zero (overflowed), go to end
+
+    ; but not zero, so...
+    ST A $0100    ; store that X to $0100
+    LD X $0100    ; load that to X
+    JP LOOP       ; not zero, so go back to LOOP
+
+END:
+    ; we just clear the stuff we use
+    LD A #00
+    ST A $0100
+
+    ; clear the white line we drew
+    LD A #00      
+    ST A $F0F0
+    ST A $F0F4
+    ST A $F0F5
+    ST A $F0F6
+    LD A #$FF
+    ST A $F0F2
     ST A $F0FF
 
-    ; loop
-    JP START
+    JP $0300      ; then we jump to the loaded sector
---
+++ asm/drawlines.asm
@@ -0,0 +1,26 @@
+#offset $0300
+
+START:
+    ; send coordinates to GPU
+    LD A $F0E9
+    ST A $F0F0
+    LD A $F0E9
+    ST A $F0F1
+    LD A $F0E9
+    ST A $F0F2
+    LD A $F0E9
+    ST A $F0F3
+
+    ; set color
+    LD A $F0E9
+    ST A $F0F4
+    LD A $F0E9
+    ST A $F0F5
+    LD A $F0E9
+    ST A $F0F6
+
+    ; draw
+    ST A $F0FF
+
+    ; loop
+    JP START
